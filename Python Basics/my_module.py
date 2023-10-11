1e0243e2d9b70c3833e3abbf8865d02d3156e940
Nuri Buğra Demir <bugranuri.demir@gmail.com>
2023-10-11 13:29:14 +0300

    modules were done

---
+++ Python Basics/my_module.py
@@ -0,0 +1,22 @@
+print("Module imported")
+
+number = 10
+
+numbers = [1, 2, 3, 4, 5]
+
+person = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
+
+def func(x):
+    """
+    This is a function
+    """
+    print(f"X is {x}")
+
+class Person:
+    def speak(self):
+        print("I am speaking")
+    
